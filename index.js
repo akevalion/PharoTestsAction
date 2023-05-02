f030c9a55d09dc1231ad4a26f372cdc28ffe055d
Milton Mamani Torres <akevalion@gmail.com>
2023-05-02 14:51:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const {execSync} = require('child_process')
+const core = require('@actions/core');
+const github = require('@actions/github');
+const {execSync} = require('child_process');
+
+const path = require('path');
+const fs = require('fs');
+
 try {
     // `repo` input defined in action metadata file
     const nameToGreet = core.getInput('removes-repo');
@@ -12,7 +16,28 @@ try {
     console.log(`The event payload: ${payload}`);
 
     console.log(execSync('curl -L https://get.pharo.org/64/alpha+vm | bash'));
-    console.log(execSync('ls -all'));
+
+    fs.readdir(__dirname, function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        //listing all files using forEach
+        files.forEach(function (file) {
+            // Do whatever you want to do with the file
+            console.log(file); 
+        });
+    });
+    console.log('...');
+    fs.readdir('.', function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        //listing all files using forEach
+        files.forEach(function (file) {
+            // Do whatever you want to do with the file
+            console.log(file); 
+        });
+    });
     //console.log('./pharo --headless Pharo.image ./scripts/runTest.st');
 
   } catch (error) {
