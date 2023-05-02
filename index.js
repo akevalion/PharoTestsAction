89d1b3d15fad9c714adb73076e30979390a4dae0
Milton Mamani Torres <akevalion@gmail.com>
2023-05-02 16:07:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,23 +5,26 @@ const {execSync} = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
+function run(command){
+    console.log('Running: '+command);
+    console.log(execSync(command));
+}
 try {
     // `repo` input defined in action metadata file
     const nameToGreet = core.getInput('removes-repo');
-    console.log(`Removing: ${nameToGreet}`);
+    
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     //const payload = JSON.stringify(github.context.payload, undefined, 2)
     //console.log(`The event payload: ${payload}`);
 
-    
-    console.log(execSync('curl -L https://get.pharo.org/64/alpha+vm | bash'));
-    const file = path.join(__dirname, 'runTest.st');
-    console.log(fs.existsSync(path));
-
-    console.log(execSync('./pharo --headless Pharo.image '+file));
-
+    //console.log(`Removing: ${nameToGreet}`);
+    run ('curl -L https://get.pharo.org/64/alpha+vm | bash')
+    const file = path.join(__dirname, '/runTest.st');
+    console.log(file);
+    console.log(fs.existsSync(file));
+    run ('./pharo --headless Pharo.image '+file);
   } catch (error) {
     core.setFailed(error.message);
   }
