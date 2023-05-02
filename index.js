32ce56c11e6be733e4efb6910128062ca23753de
Milton Mamani Torres <akevalion@gmail.com>
2023-05-02 14:17:53 +0200

    update workflow

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
-
+const {exec} = require('child_process')
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('repo');
-    console.log(`Hello ${nameToGreet}!`);
+    // `repo` input defined in action metadata file
+    const nameToGreet = core.getInput('removes-repo');
+    console.log(`Removing: ${nameToGreet}`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
+
+    exec('./run.sh', (error,stdout, stderr)=> {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    } );
+
   } catch (error) {
     core.setFailed(error.message);
   }
